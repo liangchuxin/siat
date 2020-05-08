1e527d2e8f4b3780c26bdbddff73ae3d1440bf3e
Chuxin Liang <liang81077@gapps.uwcsea.edu.sg>
2020-05-08 22:18:25 +0800

    NYAA Project

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           SIAT Project
         </a>
       </header>
     </div>
