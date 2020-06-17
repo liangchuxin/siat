273b22d484ce08ee17845fa726f433056830ec4c
Chuxin Liang <liang81077@gapps.uwcsea.edu.sg>
2020-06-18 00:10:52 +0800

    Updates

---
+++ precache-manifest.b64d204ed03a32666cda80be752055ee.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f641dee0f61d2ac801fe28fa0b6bd29b",
+    "url": "/siat/index.html"
+  },
+  {
+    "revision": "8ca31b4fb4e68dd43f00",
+    "url": "/siat/static/css/main.a9769f21.chunk.css"
+  },
+  {
+    "revision": "2f38f047b09f8e6c3c2f",
+    "url": "/siat/static/js/2.abe0ede4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/siat/static/js/2.abe0ede4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca31b4fb4e68dd43f00",
+    "url": "/siat/static/js/main.f13d5dc7.chunk.js"
+  },
+  {
+    "revision": "eb6792f7880c3d263d73",
+    "url": "/siat/static/js/runtime-main.6111a0e6.js"
+  },
+  {
+    "revision": "b3fb5130cae3fee6b075f70d89623590",
+    "url": "/siat/static/media/bear-fullwidth.b3fb5130.png"
+  },
+  {
+    "revision": "2195ef894dec00601424ed8ba87a340c",
+    "url": "/siat/static/media/bear.2195ef89.png"
+  },
+  {
+    "revision": "7d9edbcf2d005b7600414b02b51d0933",
+    "url": "/siat/static/media/elephant-fullwidth.7d9edbcf.png"
+  },
+  {
+    "revision": "80d94b82e85a0790617edb01e0ef6998",
+    "url": "/siat/static/media/elephant.80d94b82.png"
+  },
+  {
+    "revision": "dcbcc1b2fccc51dbe4c5458d9c6787ab",
+    "url": "/siat/static/media/pangolin-fullwidth.dcbcc1b2.png"
+  },
+  {
+    "revision": "1a57413da279420d2b6e7ea72174969a",
+    "url": "/siat/static/media/pangolin.1a57413d.png"
+  },
+  {
+    "revision": "cb0860dd3818707d9a5f010dbdfb9bbc",
+    "url": "/siat/static/media/rhino-fullwidth.cb0860dd.png"
+  },
+  {
+    "revision": "398ff08ab92ec18308d99c5ed8798aae",
+    "url": "/siat/static/media/rhino.398ff08a.png"
+  },
+  {
+    "revision": "e3dcc9d6e2bf427b903abb470b136fe9",
+    "url": "/siat/static/media/tiger-fullwidth.e3dcc9d6.png"
+  }
+]);
